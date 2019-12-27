8f07cae600556d252b41eb45206b58f406f3ad0d
Matthieu <matthieu974fr.pignolet@gmail.com>
2019-12-27 21:45:47 +0400

    Remove console.log & add a comment

--- src/handlers/httpErrorsHandler.js
+++ src/handlers/httpErrorsHandler.js
@@ -1,6 +1,6 @@
-
+// We load the status codes from the http module.
 module.exports.messages = require('http').STATUS_CODES
-console.log(module.messages)
+
 module.exports.response = (res, status, data) => {
 	const error = status !== 200;
 	return res.status(status).json({
